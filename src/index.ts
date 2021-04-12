4e9a1181b95a8a1d168f12ac51faab575c6016a8
trigger <triggerhappy187@live.com>
2021-04-12 14:43:16 -0700

    chat cmd, hide locked, tag lfg

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import Discord, { TextChannel } from "discord.js";
+import Discord, { Message, TextChannel } from "discord.js";
 import https from "https";
 import { MgoGameMode, MgoGameModeNames, MgoMap, MgoMapNames, MgoMode, MgoModeNames } from "./constants";
 
@@ -6,12 +6,26 @@ const client = new Discord.Client();
 const clientToken = "";
 const cacheMessages: Record<number, Discord.Message> = {};
 const cacheLobbies: Record<number, GameLobby> = {};
+const channelId = "817184487061454928";
+let latestGameData: APIGames | undefined;
 
 // Login to discord
 client.login(clientToken);
 client.on('ready', () => {
   console.log(`Logged in as ${client.user?.tag}!`);
   setInterval(queryLobbies, 5000);
+});
+
+// Handle DMs
+client.on('message', (msg: Message) => {
+  if (msg.content.trim() === "!players" || msg.content.trim() === "!games") {
+    const gameCount = latestGameData?.lobbies.length;
+    if (gameCount === 0) {
+      msg.channel.send(`There are currently no games being played.`);
+    } else {
+      msg.channel.send(`There are currently ${latestGameData?.players} players in ${latestGameData?.lobbies.length} ${gameCount === 1 ? "game" : "games"} on MGO2PC. <https://mgo2pc.com/games>`);
+    }
+  }
 });
 
 const getGameEmbed = (lobby: GameLobby, closed = false) => {
@@ -29,18 +43,25 @@ const getGameEmbed = (lobby: GameLobby, closed = false) => {
   const embed = new Discord.MessageEmbed()
     //`${lobby.name} (${lobby.players.length}/${lobby.maxPlayers})`
     .setTitle(title)
-    .setURL("https://mgo2pc.com/games")
-    .setDescription(lobby.comment.replace(/(\r\n|\n|\r)/gm, " "))
-    .setThumbnail(`https://mgo2pc.com/static/media/maps/${currentGameData[1]}.jpg`)
-    .addField('Map', `${map}`, true)
-    .addField('Mode', `${gameMode}`, true)
 
-  let playerList = '';
-  for (const player of lobby.players) {
-    playerList += `${player.name}\n`;
+  if (!closed) {
+    embed.setDescription(' ')
+      .setURL("https://mgo2pc.com/games")
+      .setThumbnail(`https://mgo2pc.com/static/media/maps/${currentGameData[1]}.jpg`)
+      .addField('Map', `${map}`, true)
+      .addField('Mode', `${gameMode}`, true)
+
+    let playerList = '';
+    for (const player of lobby.players) {
+      playerList += `${player.name}\n`;
+    }
+
+    embed.addField(`Players (${lobby.players.length}/${lobby.maxPlayers})`, playerList);
+  } else {
+    embed.addField('Host', `${host?.name}`, true)
+    embed.setDescription(`This lobby is now closed.`)
   }
 
-  embed.addField(`Players (${lobby.players.length}/${lobby.maxPlayers})`, playerList);
   embed.setFooter('Powered by https://mgo2pc.com', `https://mgo2pc.com/static/media/icon.png`);
 
   return embed;
@@ -60,17 +81,21 @@ const queryLobbies = () => {
         const json: APIResponse<APIGames> = JSON.parse(d.toString());
         const lobbies = json.data.lobbies;
 
+        latestGameData = json.data;
+
         // update active lobbies
         for (const lobby of lobbies) {
-          const channel = client.channels.cache.get("817184487061454928") as TextChannel;
+          if (lobby.locked) continue;
+
+          const channel = client.channels.cache.get(channelId) as TextChannel;
           const embed = getGameEmbed(lobby);
 
           if (!cacheMessages[lobby.id]) {
-            const message = await channel.send(embed);
+            const message = await channel.send('<@&818099974935805972>', embed);
             cacheMessages[lobby.id] = message;
           } else {
             const message = cacheMessages[lobby.id];
-            message.edit(embed);
+            message.edit('<@&818099974935805972>', embed);
           }
 
           cacheLobbies[lobby.id] = lobby;
